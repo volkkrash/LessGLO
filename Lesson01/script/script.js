b63586eb30b13293587c9105ebf30168cbea0991
Vladislav Vlasenko <dev-spark@ukr.net>
2019-08-26 13:19:32 +0300

    Урок №2

--- Lesson01/script/script.js
+++ Lesson01/script/script.js
@@ -1,4 +1,22 @@
 
-let money, income, addExpenses, deposit, mission, period;
+let money = 25000,
+    income = "Фриланс",
+    addExpenses = "Транспорт, Обучение, Дом",
+    deposit = false,
+    mission = 120000,
+    period = 6,
+    daysInMonth = 30;
 
-alert('Hello GLO!');
+console.log(typeof(money));
+console.log(typeof(income));
+console.log(typeof(deposit));
+console.log(income.length);
+
+console.log("За " + period + " месяцев необходимо заработать " + mission + " гривен.");
+
+console.log(addExpenses.toLowerCase().split(', '));
+
+let budgetDay = money / daysInMonth;
+
+console.log('budgetDay(результат): ', budgetDay);
+console.log('budgetDay(остаток): ', money % daysInMonth);
