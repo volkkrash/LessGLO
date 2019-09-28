1d448b726703696ddde5648c5ec5d81acb7e7e58
Vladislav Vlasenko <dev-spark@ukr.net>
2019-09-28 19:39:36 +0300

    Урок 15

--- 3dGlo/script/script.js
+++ 3dGlo/script/script.js
@@ -241,4 +241,21 @@ document.addEventListener('DOMContentLoaded', () => {
   };
   slider();
 
+  const imageReplacement = () => {
+    let command = document.querySelector('.command'),
+        defaultSrc = '';
+    command.addEventListener('mouseover', (e) => {
+      if (e.target.className === 'command__photo') {
+        defaultSrc = e.target.src;
+        e.target.src = e.target.dataset.img;
+      }
+    });
+    command.addEventListener('mouseout', (e) => {
+      if (e.target.className === 'command__photo') {
+        e.target.src = defaultSrc;
+        defaultSrc = '';
+      }
+    });
+  };
+  imageReplacement();
 });
