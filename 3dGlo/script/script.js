0626027cabf0ff9d47729344c8f733660d383a3e
Vladislav Vlasenko <dev-spark@ukr.net>
2019-09-29 20:53:52 +0300

    Урок 19

--- 3dGlo/script/script.js
+++ 3dGlo/script/script.js
@@ -327,12 +327,12 @@ document.addEventListener('DOMContentLoaded', () => {
         item.appendChild(statusMessage);
         statusMessage.textContent = loadMessage;
         const formData = new FormData(item);
-        let body = {};
-        formData.forEach((val, key) => {
-          body[key] = val;
-        });
-        postData(body)
-          .then (() => {
+        
+        postData(formData)
+          .then ((response) => {
+            if (response.status !== 200) {
+              throw new Error ('Status network not 200');
+            }
             statusMessage.textContent = successMessage;
             let clearD = item.querySelectorAll('input');
             clearD.forEach((item) => {
@@ -346,26 +346,8 @@ document.addEventListener('DOMContentLoaded', () => {
       });
     });
 
-
-    const postData = (body) => {
-      return new Promise ((resolve, reject) => {
-        const request = new XMLHttpRequest();
-
-        request.addEventListener('readystatechange', () => {
-          if (request.readyState !== 4) {
-            return;
-          }
-          if (request.status === 200) {
-            resolve();
-          } else {
-            reject(request.status);
-          }
-        });
-
-        request.open('POST', './server.php');
-        request.setRequestHeader('Content-Type', 'application/json');
-        request.send(JSON.stringify(body));
-      });
+    const postData = (formData) => {
+      return fetch('./server.php', {method: 'POST', headers: {'Content-Type': 'application/json'}, body: formData});
     };
   };
   sendForm();
